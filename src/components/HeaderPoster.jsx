143183e3299fec36b003f172044e31fd4abd3287
elnazkoupaei <koupaei.elnaz@gmail.com>
2025-04-30 09:29:11 +0200

    Edit headerPoster

--- src/components/HeaderPoster.jsx
+++ src/components/HeaderPoster.jsx
@@ -1,8 +1,18 @@
-function HeaderPoster({posterTitle,posterSubtitle}) {
+import idPhoto from '../assets/idPhoto.jpg';
+
+function HeaderPoster({posterTitle,posterSubtitle,name,race,parentName,age,story}) {
 
     return (<div>
+
+        <img src={idPhoto} alt='Grand-doggy'></img>
          <h1>{posterTitle}</h1>
          <p>{posterSubtitle}</p>
+         <p>{name}</p>
+         <p>{race}</p>
+         <p>{parentName}</p>
+         <p>{age}</p>
+         <p>{story}</p>
+
     </div>)
    
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ import HeaderPoster from "./HeaderPoster";
 function Home(){
 return <div>
 
-    <div>homepage</div>
-    <HeaderPoster posterTitle="Hello Polo!"  posterSubtitle="I'm a cute dog!"/>
-    <HeaderPoster posterTitle="Hello Body!"  posterSubtitle="I'm a lost dog!"/>
+   
+    <HeaderPoster posterTitle="Hello Polo!"  posterSubtitle="I'm a cute dog!" name="Polo" race="Bischon-Frise" parentName="Minoo" age="2.5 years old" story="This is my story!" />
+    
 
 
 </div>
